c05a32c8268557420d67f432d8cf33d15f865ee1
Maxim Segal <mxseg@yahoo.com>
2023-05-27 14:20:10 +0300

    Updates

---
+++ precache-manifest.455e047c75e064dbe2c75716ca821598.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb83888a1449f63a265912cf8323d7e0",
+    "url": "/Doomsday-algorithm-practice/index.html"
+  },
+  {
+    "revision": "034002c3c644f7e396ec",
+    "url": "/Doomsday-algorithm-practice/static/css/main.e8e61451.chunk.css"
+  },
+  {
+    "revision": "abaf53b0c6611c07d1cd",
+    "url": "/Doomsday-algorithm-practice/static/js/2.afda636e.chunk.js"
+  },
+  {
+    "revision": "034002c3c644f7e396ec",
+    "url": "/Doomsday-algorithm-practice/static/js/main.74419de5.chunk.js"
+  },
+  {
+    "revision": "ba9a99ca48f48f2c5154",
+    "url": "/Doomsday-algorithm-practice/static/js/runtime-main.ee6c00cc.js"
+  }
+]);
